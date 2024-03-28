702ae0f87986872ff468341ad4cc796815b9dd77
Blagoi4 <vladislavblagoiwork@gmail.com>
2024-03-28 16:00:10 +0200

    render and create Todos Component

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,24 +1,32 @@
-import MainScreen from "../components/MainScreen"
+import { useState, useEffect } from "react";
+import MainScreen from "../components/MainScreen";
+import TodosCard from "../components/TodosCard";
 
+const Todos = () => {
+  const [dataTodos, setDataTodos] = useState([]);
 
+  useEffect(() => {
+    function fetchDataTodos() {
+      fetch("https://jsonplaceholder.typicode.com/todos?_limit=15")
+        .then((response) => response.json())
+        .then((json) => setDataTodos(json));
+    }
+    fetchDataTodos();
+  }, []);
+  console.log(dataTodos);
 
-const Todos =() => {
-   const [data, setData] = ([])
-   fetch( 'https://jsonplaceholder.typicode.com/todos?_limit=30' )
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-    return(
-        <>
-          <MainScreen heading='Pages todos' subheading='first pages'/>
-        <section className="container">
-      
+  return (
+    <>
+      <MainScreen heading="Pages todos" subheading="first pages" />
+      <section className="container">
+        <div className="todos-wrapper">
+          {dataTodos.map((item) => (
+            <TodosCard title={item.title} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
 
-            <div>Test</div>
-
-        </section>
-        </>
-    )
-}
-
-
-export default Todos
+export default Todos;
