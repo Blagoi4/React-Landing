4cf016f91eea1ddeb625805f8f5d8148b9a92305
Blagoi4 <vladislavblagoiwork@gmail.com>
2024-03-28 16:16:56 +0200

    add Spinner to component TodosCard

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
 import MainScreen from "../components/MainScreen";
 import TodosCard from "../components/TodosCard";
+import Spinner from "../components/Spinner";
 
 const Todos = () => {
   const [dataTodos, setDataTodos] = useState([]);
+  const [dataLoadingTodos, setDataLoadingTodos] = useState(false);
 
   useEffect(() => {
+    setDataLoadingTodos(true);
     function fetchDataTodos() {
       fetch("https://jsonplaceholder.typicode.com/todos?_limit=15")
         .then((response) => response.json())
         .then((json) => setDataTodos(json));
+      setDataLoadingTodos(false);
     }
     fetchDataTodos();
   }, []);
@@ -18,13 +22,17 @@ const Todos = () => {
   return (
     <>
       <MainScreen heading="Pages todos" subheading="first pages" />
-      <section className="container">
-        <div className="todos-wrapper">
-          {dataTodos.map((item) => (
-            <TodosCard title={item.title} />
-          ))}
-        </div>
-      </section>
+      {dataLoadingTodos ? (
+        <Spinner />
+      ) : (
+        <section className="container">
+          <div className="todos-wrapper">
+            {dataTodos.map((item) => (
+              <TodosCard title={item.title} key={item.id} />
+            ))}
+          </div>
+        </section>
+      )}
     </>
   );
 };
