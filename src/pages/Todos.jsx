d1f4f41eb9d2457f5c776740ef45102094c4941a
Blagoi4 <vladislavblagoiwork@gmail.com>
2024-03-28 12:21:09 +0200

    use JsonPlaceHolder API, production 30 query

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -3,6 +3,10 @@ import MainScreen from "../components/MainScreen"
 
 
 const Todos =() => {
+   const [data, setData] = ([])
+   fetch( 'https://jsonplaceholder.typicode.com/todos?_limit=30' )
+  .then((response) => response.json())
+  .then((json) => console.log(json));
     return(
         <>
           <MainScreen heading='Pages todos' subheading='first pages'/>
