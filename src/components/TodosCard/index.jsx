fcf514e636de68e1eb020030e1068e831c3a0341
Blagoi4 <vladislavblagoiwork@gmail.com>
2024-03-29 16:44:01 +0200

    create logic Search in Todos

--- src/components/TodosCard/index.jsx
+++ src/components/TodosCard/index.jsx
@@ -3,10 +3,13 @@ import './todos.css'
 
 const TodosCard = ({title}) => {
     return (
+      
     <div className='todos-card'>
          <div className="todos"> 
         {title}
           </div>
+
+           
     </div>
         )
 }
--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -6,33 +6,55 @@ import Spinner from "../components/Spinner";
 const Todos = () => {
   const [dataTodos, setDataTodos] = useState([]);
   const [dataLoadingTodos, setDataLoadingTodos] = useState(false);
+  const [value, setValue] = useState("");
+
 
   useEffect(() => {
     setDataLoadingTodos(true);
+
     function fetchDataTodos() {
       fetch("https://jsonplaceholder.typicode.com/todos?_limit=15")
         .then((response) => response.json())
         .then((json) => setDataTodos(json));
       setDataLoadingTodos(false);
     }
+
     fetchDataTodos();
   }, []);
-  console.log(dataTodos);
+
+  const errorMassage = "Error";
+  const filteredTodos = dataTodos.filter((el) => {
+    if (el.title === value || el.title) {
+      return el.title.toLowerCase().includes(value.toLowerCase());
+    } else if (el.title != value && value.length > 0) {
+      return console.log("errorMassage");
+    }
+  });
+
 
   return (
     <>
       <MainScreen heading="Pages todos" subheading="first pages" />
-      {dataLoadingTodos ? (
-        <Spinner />
-      ) : (
-        <section className="container">
-          <div className="todos-wrapper">
-            {dataTodos.map((item) => (
+
+      <section className="container">
+        <div className="todos-wrapper">
+          <div className="todos-block">
+            {filteredTodos.map((item) => (
+                
               <TodosCard title={item.title} key={item.id} />
             ))}
           </div>
-        </section>
-      )}
+
+          <div className="todos-search">
+            <input
+              default={value}
+              onChange={(event) => setValue(event.target.value)}
+              type="text"
+            />
+           {/* {value.length >= 3 && dataTodos.title !== value ? errorMassage : ''} */}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
